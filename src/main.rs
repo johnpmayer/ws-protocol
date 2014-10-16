ced7efc8efbb5409ce3d9e7c265f94e117abb405
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2014-10-15 23:19:59 -0400

    pull back from SSL

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,6 @@ extern crate openssl;
 
 use std::io::{TcpListener, TcpStream};
 use std::io::{Acceptor, Listener};
-use std::io::fs::{PathExtensions};
-use openssl::x509::{PEM};
-use openssl::ssl::{SslContext, Tlsv1, SslStream};
 
 fn main() {
 
@@ -13,45 +10,22 @@ fn main() {
 
     let mut acceptor = listener.listen();
 
-    fn handle_client(mut stream: SslStream<TcpStream>) {
-        println!("Handling secure client");
+    fn handle_client(stream: TcpStream) {
+        println!("Handling client");
     }
 
-    match SslContext::new(Tlsv1) {
-        Err(e) => {
-            println!("SslContext error: {}", e);
-        }
-        Ok(mut ctx) => { 
+    /*
+     * Accept Loop
+     */
 
-            ctx.set_CA_file(&Path::new(""));
-            ctx.set_certificate_file(&Path::new(""), PEM);
-
-            /*
-             * Accept Loop
-             */
-
-            for stream_result in acceptor.incoming() {
-                match stream_result {
-                    Err(e) => { 
-                        println!("Accept error: {}", e); 
-                    }
-                    Ok(mut stream) => spawn(proc() {
-                        println!("Securing client from {}", stream.peer_name());
-                        let ssl_result = SslStream::new(&ctx, stream);
-                        match ssl_result {
-                            Err(e) => {
-                                println!("Ssl error: {}", e);
-                            }
-                            Ok(secure_stream) => {
-                                handle_client(secure_stream)
-                            }
-                        }
-                    })
-                }
+    for stream_result in acceptor.incoming() {
+        match stream_result {
+            Err(e) => { 
+                println!("Accept error: {}", e); 
             }
-
-            drop(acceptor);
-
+            Ok(stream) => spawn(proc() {
+                handle_client(stream)
+            })
         }
-    };
+    }
 }
