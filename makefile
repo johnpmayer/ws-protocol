7d2fb65e8c41c54a3ca1858d186fd27afbe48afa
John P Mayer <john.p.mayer.jr@gmail.com>
2014-10-19 00:06:14 -0400

    Can read headers from non-secure web socket request

--- makefile
+++ makefile
@@ -1,3 +1,13 @@
 
-all:
+all: exe
+
+clean:
+	cargo clean
+
+run: exe
+	cargo run
+
+exe: target\ws-server.exe
+
+target\ws-server.exe: src\main.rs
 	cargo build
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,44 @@
 
-extern crate openssl;
+#![crate_id="rsfix#0.0"]
+#![feature(macro_rules)]
 
-use std::io::{TcpListener, TcpStream};
-use std::io::{Acceptor, Listener};
+use std::fmt;
+use std::io::{BufferedReader, TcpListener, TcpStream, Acceptor, Listener, IoResult};
+
+macro_rules! some( ($e:expr) => (match $e { Some(e) => e, None => return None } ))
 
 fn main() {
+  match run() {
+    Ok(_) => return,
+    Err(e) => {
+      println!("Got error {}", e);
+    }
+  }
+}
+
+struct Header {
+    name: String,
+    value: String
+}
+
+impl fmt::Show for Header {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Header({} => {})", self.name, self.value)
+    }
+}
+
+fn read_header(line: &str) -> Option<Header> {
+    let mut header = line.trim_right().split_str(":");
+    let header_name = some!(header.next());
+    let header_value = some!(header.next()).trim_left();
+    return Some(Header { 
+      name: some!(from_str(header_name)),
+      value: some!(from_str(header_value))
+    });
+}
+
+
+fn run() -> IoResult<int> {
 
     let listener = TcpListener::bind("0.0.0.0", 9000);
 
@@ -12,6 +46,28 @@ fn main() {
 
     fn handle_client(stream: TcpStream) {
         println!("Handling client");
+        
+        let mut reader = BufferedReader::new(stream);
+        
+        let method = reader.read_line();
+        
+        loop {
+            match reader.read_line() {
+                Err(e) => {
+                    println!("Read error: {}", e);
+                    break
+                },
+                Ok(line) => {
+                    match read_header(line.as_slice()) {
+                        None => break,
+                        Some(header) => println!("Header '{}'", header)
+                    }
+                }
+            }
+        }
+        
+        println!("Finished reading headers");
+        
     }
 
     /*
@@ -19,6 +75,7 @@ fn main() {
      */
 
     for stream_result in acceptor.incoming() {
+        
         match stream_result {
             Err(e) => { 
                 println!("Accept error: {}", e); 
@@ -28,4 +85,7 @@ fn main() {
             })
         }
     }
+    
+    return Ok(0);
 }
+
