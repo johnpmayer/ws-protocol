98791eb1bb91dde11e059698ab84cc182102131c
John P Mayer <john.p.mayer.jr@gmail.com>
2014-10-21 00:00:00 -0400

    whoo macros are the shit

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,12 @@ use crypto::sha1::Sha1;
 use serialize::base64::{Config, Standard, ToBase64};
 use std::collections::TreeMap;
 use std::fmt;
-use std::io::{BufferedReader, TcpListener, TcpStream, Acceptor, Listener, IoResult};
+use std::io::{Acceptor, BufferedReader, IoError, IoResult, Listener, TcpListener, TcpStream, OtherIoError};
+use std::num::FromPrimitive;
 
-macro_rules! some( ($e:expr) => (match $e { Some(e) => e, None => return None } ))
+macro_rules! some( ($e:expr,$none:expr) => (match $e { Some(e) => e, None => return $none } ))
+macro_rules! ok( ($e:expr,$none:expr) => (match $e { ok(x) => x, Err(e) => return $none } ))
+macro_rules! genericError( ($msg:expr) => (Err(IoError { kind: OtherIoError, desc: $msg, detail: None })))
 
 static WS_GUID : &'static str = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
@@ -37,22 +40,43 @@ impl fmt::Show for Header {
 
 fn read_header(line: &str) -> Option<Header> {
     let mut header = line.trim_right().split_str(":");
-    let header_name = some!(header.next());
-    let header_value = some!(header.next()).trim_left();
+    let header_name = some!(header.next(), None);
+    let header_value = some!(header.next(), None).trim_left();
     return Some(Header { 
-      name: some!(from_str(header_name)),
-      value: some!(from_str(header_value))
+      name: some!(from_str(header_name), None),
+      value: some!(from_str(header_value), None)
     });
 }
 
+fn send(mut stream: TcpStream, msg: &[u8]) -> IoResult<()> {
 
-fn run() -> IoResult<int> {
+    try!(stream.write([129]));
+        
+    let len: uint = msg.len();
+    let l1: u8 = some!(FromPrimitive::from_uint(len >> 24), genericError!("Conversion error"));
+    let l2: u8 = some!(FromPrimitive::from_uint(len >> 16), genericError!("Conversion error"));
+    let l3: u8 = some!(FromPrimitive::from_uint(len >>  8), genericError!("Conversion error"));
+    let l4: u8 = some!(FromPrimitive::from_uint(len      ), genericError!("Conversion error"));
+    
+    try!(match len {
+        _ if len <= 125 => 
+            stream.write(&[l4]),
+        _ if len > 125 && len <= 65535 => 
+            stream.write(&[126u8, l3, l4]),
+        _ => 
+            stream.write(&[127u8, l1, l2, l3, l4])
+    });
+
+    return stream.write(msg)
+}
+
+fn run() -> IoResult<()> {
 
     let listener = TcpListener::bind("0.0.0.0", 9000);
 
     let mut acceptor = listener.listen();
 
-    fn handle_client(mut stream: TcpStream) {
+    fn handle_client(mut stream: TcpStream) -> IoResult<()> {
         println!("Handling client");
         
         let mut reader = BufferedReader::new(stream.clone());
@@ -84,7 +108,7 @@ fn run() -> IoResult<int> {
         // TODO : validate "Origin" header. important for security
         
         let challenge_response: String = match headers.find(&key) {
-            None => return println!("No challenge header"),
+            None => return genericError!("No challenge header"),
             Some(challenge) => {
                 let mut hasher = Sha1::new();
                 let mut local = challenge.clone();
@@ -105,6 +129,8 @@ fn run() -> IoResult<int> {
         
         let _res = stream.write(format_args!(fmt::format, "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: {}\r\n\r\n", challenge_response).as_bytes());
         
+        send(stream, "Test message".as_bytes())
+        
     }
 
     /*
@@ -118,11 +144,14 @@ fn run() -> IoResult<int> {
                 println!("Accept error: {}", e); 
             }
             Ok(stream) => spawn(proc() {
-                handle_client(stream)
+                match handle_client(stream) {
+                    Ok(()) => println!("Client quit without error"),
+                    Err(e) => println!("Client exited with error: {}", e)
+                };
             })
         }
     }
     
-    return Ok(0);
+    return Ok(());
 }
 
